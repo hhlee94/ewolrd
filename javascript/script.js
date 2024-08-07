ebaf60a5019e864d8d73f1576114a2c695fdf4b6
hhlee94 <hhlee94@naver.com>
2024-08-07 09:59:22 +0900

    최종본

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+$(function () {
+    // image slide
+    const slideList = $(".slide-list");
+    const slideInit = () => {
+     slideList.css({ marginLeft: 0 }).find("li:first").appendTo(slideList);
+    };
+   
+    const moveSlide = () => {
+     slideList.animate({ marginLeft: "-100%" }, 1000, slideInit);
+    };
+   
+    setInterval(moveSlide, 3000);
+   
+    // modal
+    $(".notice li:first").click((e) => {
+     e.preventDefault();
+     $("#modal").addClass("active");
+    });
+   
+    $(".modal-btn").click(() => {
+     $("#modal").removeClass("active");
+    });
+   });
+   
